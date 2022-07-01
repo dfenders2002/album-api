7dc39d5cd44d23f046cec0e9540275f0b4eb546a
Daan Enderrs <daan@posonas.com>
2022-07-01 20:14:36 +0200

    Voor week 6

--- Album.Api/Startup.cs
+++ Album.Api/Startup.cs
@@ -31,6 +31,7 @@ namespace Album.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddScoped<IAlbumService, AlbumService>();
             services.AddSwaggerGen(c =>
@@ -44,6 +45,10 @@ namespace Album.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(policy => policy
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowAnyOrigin());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
